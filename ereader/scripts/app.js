d7ba1d7baab24037e5a595736ecd2815b720806f
Belén Albeza <belen.albeza@gmail.com>
2012-11-13 11:59:15 +0000

    FIX bug switching pages

--- ereader/scripts/app.js
+++ ereader/scripts/app.js
@@ -10,9 +10,9 @@ function App() {
 
 App.prototype.switchTo = function(pageId) {
     // reattach current pages to the Pages container
-    var pages = document.getElementsByClassName('page');
-    for (var i = 0; i < pages.lengh++; i++) {
-        this.pagesContainer.appendChild(pages);
+    var pages = this.currentContainer.getElementsByClassName('page');
+    for (var i = 0; i < pages.length++; i++) {
+        this.pagesContainer.appendChild(pages[i]);
     }
     // attach target page to Current Page container
     var target = document.getElementById(pageId);
